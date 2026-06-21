8b575ef4417a842a1e9dfb67d3cddb8bb2e559e2
lohitham2727 <lohitham2727@gmail.com>
2026-06-21 15:33:08 +0530

    Added Swagger documentation and production enhancements

---
+++ account-service/src/main/java/com/accountservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.accountservice.exception;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+}
---
+++ event-gateway/src/main/java/com/event/gateway/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.event.gateway.exception;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+}
